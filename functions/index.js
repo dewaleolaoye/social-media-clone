7e9d5bbcbdd1a084ed8ae7f05dc3c39b9007519b
dewaleolaoye <adewaleolaoye21@gmail.com>
2019-10-14 15:54:04 +0100

    add screams

--- functions/index.js
+++ functions/index.js
@@ -28,4 +28,28 @@ exports.getScreams = functions.https.onRequest((req, res) => {
     });
 });
 
-exports.createScream = functions.https.onRequest((req, res) => {});
+exports.createScream = functions.https.onRequest((req, res) => {
+  // const { body, userHandle } = req.body;
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+  };
+
+  admin
+    .firestore()
+    .collection('screams')
+    .add(newScream)
+    .then(doc => {
+      return res.status(201).json({
+        status: 'success',
+        message: `document ${doc.id} created successfully`
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: 'something went wrong'
+      });
+      console.log('the error is here', error);
+    });
+});
