71d1d2e2865ec7884db2ec06eacf4b2a509b8a42
dewaleolaoye <adewaleolaoye21@gmail.com>
2019-10-15 11:00:35 +0100

    validate user signup

--- functions/index.js
+++ functions/index.js
@@ -54,9 +54,23 @@ app.post('/scream', (req, res) => {
       console.log('the error is here', error);
     });
 });
+/**
+ * TODO: Move this to another file
+ */
+
+// Helper Function
+const isEmail = email => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(regEx)) return true;
+  else return false;
+};
+
+const isEmpty = string => {
+  if (string.trim() === '') return true;
+  else return false;
+};
 
 // Signup
-
 app.post('/signup', (req, res) => {
   const { email, password, confirmPassword, userHandle } = req.body;
   const newUser = {
@@ -66,6 +80,32 @@ app.post('/signup', (req, res) => {
     userHandle
   };
 
+  /**
+   * TODO: I will refactor everything to better standard
+   *! where I wiill refactor and seperate concerns
+   ** REPETITION OF CODE
+   */
+  // Validations
+  let errors = {};
+
+  if (isEmpty(email)) {
+    errors.email = 'Must not be empty';
+  } else if (!isEmail(email)) {
+    errors.email = 'Must be a valid email address';
+  }
+
+  if (isEmpty(password)) errors.password = 'Must not be empty';
+
+  if (password !== confirmPassword)
+    errors.confirmPassword = 'Password must match';
+
+  if (isEmpty(userHandle)) errors.userHandle = 'Must not be empty';
+
+  if (Object.keys(errors).length > 0) {
+    return res.status(400).json(errors);
+  }
+
+  let userId, token;
   db.doc(`/users/${userHandle}`)
     .get()
     .then(doc => {
@@ -80,11 +120,25 @@ app.post('/signup', (req, res) => {
         .auth()
         .createUserWithEmailAndPassword(newUser.email, newUser.password)
         .then(data => {
-          // console.log(data.user);
+          userId = data.user.uid;
           return data.user.getIdToken();
         })
-        .then(token => {
-          return res.status(201).json({ token });
+        .then(idToken => {
+          token = idToken;
+          const userCredentials = {
+            userHandle,
+            email,
+            createdAt: new Date().toISOString(),
+            userId
+          };
+
+          return db.doc(`/users/${userHandle}`).set(userCredentials);
+        })
+        .then(() => {
+          return res.status(201).json({
+            message: 'Successfully signed up',
+            token
+          });
         })
         .catch(error => {
           if ((error.code = 'auth/email-already-in-use')) {
